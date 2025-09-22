7d983b546bdffcacd30dda582c45f6349b372bbe
shrii-ed94 <sherine.edel@efrei.net>
2025-09-22 00:25:41 -0400

    cette fois ci envoi du code sur la partie js + suppression du message test

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-alert("Le jeu démarre !");
+// Création de lagrille 5x5
+let size = 5;
+let grid = [];
+
+for (let i = 0; i <= size; i++) { 
+    grid[i] = [];
+    for (let j = 0; j < size; j++) {
+        grid[i][j] = "~"; // représentation de l'eau
+    }
+}
+
+// fonction placer un bateau
+function placeBoat(x, y) {
+    if(x < 0 || y < 0 || x > size || y > size) { 
+        alert("Position invalide !");
+        return;
+    }
+    grid[x][y] = "B";
+}
+
+// fonction tire sur la grille
+function shoot(x, y) {
+    let cell = grid[x][y];
+    if(cell === "B") {
+        grid[x][y] = "X";
+        return treu;
+    } else {
+        grid[x][y] = "O";
+        return false;
+    }
+}
+
+// affichage de la grille dans le HTML
+function printGrid() {
+    let gameDiv = document.getElementById("game");
+    gameDiv.innerHTML = "";
+    for (let row of grid) {
+        gameDiv.innerHTML += row.join(" ") + "<br>";
+    }
+}
+
+// test du jeu
+placeBoat(1,1);
+shoot(2,2);
+printGrid();
